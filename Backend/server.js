e0cc9e071ac074053abc5228558df32d4862b0dd
TheSirDingle <deathawk1998@gmail.com>
2025-10-10 22:33:59 -0500

    Setup backend and front end test

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 import http, {ServerResponse} from "http";
 import dotenv from 'dotenv';
+import { json } from "stream/consumers";
+import { time } from "console";
 
 dotenv.config();
 
@@ -9,7 +11,21 @@ const backendPort = process.env.backendPort;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
 
-    // 
+    const {method, url} = req;
+
+    // creates main url for path splicing
     const parsedUrl = new URL(req.url, baseURL);
+    // general query parameter variable for later access
     const queryParams = parsedUrl.searchParams;
+
+    if (parsedUrl.pathname === "/test" && method === "GET") {
+        let currentTime = new Date();
+        res.end(JSON.stringify({message: 'Hello Test, testing at: ' + currentTime.toString()}));
+        res.statusCode = 200;
+    }
+})
+
+const port = backendPort;
+server.listen(port, () => {
+    console.log(`Server running at ${baseURL}:${port}`);
 })
