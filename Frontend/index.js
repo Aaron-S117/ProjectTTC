c1b15666dfe34b4cf8541776242d94a08386cf5e
TheSirDingle <deathawk1998@gmail.com>
2025-10-11 23:27:34 -0500

    reintroduced async and separate file for handling login

--- Frontend/index.js
+++ Frontend/index.js
@@ -6,41 +6,12 @@ function verifyLogin() {
     return 'test';
 }
 
-function HandleAccountCreation() {
+async function HandleAccountCreation() {
     const loginDiv = document.getElementById('login');
     loginDiv.remove();
 
-    createAccountRegPage();
-}
-
-function createAccountRegPage() {
-    console.log("Test Import");
-
-    const mainElement = document.createElement('div')
-    mainElement.setAttribute('id', 'CARMain');
-    const body = document.getElementsByTagName('body')[0];
-    body.appendChild(mainElement);
-
-
-    carFormCreation(mainElement);
-
-}
-
-function carFormCreation(mainElm) {
-
-    const username = document.createElement('input');
-    username.setAttribute('id', 'username');
-    username.setAttribute('type', 'text');
-    mainElm.appendChild(username);
-
-    const password = document.createElement('input');
-    password.setAttribute('id', 'password');
-    password.setAttribute('type', 'text');
-    mainElm.appendChild(password);
-
-    const createButton = document.createElement('button');
-    createButton.setAttribute('id', 'createButton');
-    mainElm.appendChild(createButton);
+    const loginImport = await import('./login.js');
+    loginImport.createAccountRegPage();
 }
 
 const CARButton = document.getElementById("CreateAccountLink");
