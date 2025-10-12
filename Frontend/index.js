6afa7586b6f3bee9102ec0a1a161689cca01cb6b
TheSirDingle <deathawk1998@gmail.com>
2025-10-11 23:26:38 -0500

    empty href in <a> tag was causing issues with page reloading

--- Frontend/index.js
+++ Frontend/index.js
@@ -6,12 +6,41 @@ function verifyLogin() {
     return 'test';
 }
 
-async function HandleAccountCreation() {
+function HandleAccountCreation() {
     const loginDiv = document.getElementById('login');
     loginDiv.remove();
 
-    const loginImport = await import('./login.js');
-    const CAR = loginImport.createAccountRegPage();
+    createAccountRegPage();
+}
+
+function createAccountRegPage() {
+    console.log("Test Import");
+
+    const mainElement = document.createElement('div')
+    mainElement.setAttribute('id', 'CARMain');
+    const body = document.getElementsByTagName('body')[0];
+    body.appendChild(mainElement);
+
+
+    carFormCreation(mainElement);
+
+}
+
+function carFormCreation(mainElm) {
+
+    const username = document.createElement('input');
+    username.setAttribute('id', 'username');
+    username.setAttribute('type', 'text');
+    mainElm.appendChild(username);
+
+    const password = document.createElement('input');
+    password.setAttribute('id', 'password');
+    password.setAttribute('type', 'text');
+    mainElm.appendChild(password);
+
+    const createButton = document.createElement('button');
+    createButton.setAttribute('id', 'createButton');
+    mainElm.appendChild(createButton);
 }
 
 const CARButton = document.getElementById("CreateAccountLink");
